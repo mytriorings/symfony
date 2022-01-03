7fbd05282dabc69fc5f2ff788b7852b2a4263811
kamil <kamil.dratwa@gmail.com>
2022-01-03 16:43:37 -0500

    Format

--- rebuild.sh
+++ rebuild.sh
@@ -1,4 +1,3 @@
-rm -rf ./vendor \
-  && docker-compose up -d --build --force-recreate --remove-orphans \
+docker-compose up -d --build --force-recreate --remove-orphans \
   && docker exec medial_php_apache composer install \
   && docker exec medial_php_apache composer update
--- run.sh
+++ run.sh
@@ -1,4 +1,3 @@
 docker-compose up -d \
   && docker exec medial_php_apache composer install \
   && docker exec medial_php_apache composer update
-  
